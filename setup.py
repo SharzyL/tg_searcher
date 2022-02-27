3a91c30d5d8a26650c1e793777b9db48e85c8d92
SharzyL <me@sharzy.in>
2022-02-27 15:49:45 +0800

    bump to 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 setup(
     name='tg-searcher',
